516d1d55b59d26bbf3c7dd9e89fad1f4b1431252
asus test <msa-99@outlook.com>
2024-04-29 08:44:19 +0600

    pattern explain

--- patternN++.c
+++ patternN++.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
-#include<limits.h>
-#include<string.h>
 int main(){
-    
+    int n;
+    scanf("%d",&n);
+    //start of single line print
+    for(int i=1; i<=n; i++){
+        //start for single star print
+        for(int j=1; j<=i; j++){
+            printf("*");
+        }
+        //end of single star print
+        // k++;
+        printf("\n");
+    }
+    //end for single line print
     return 0;
 }
